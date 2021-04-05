821749632cfe595b67ed238a1ae4dedf702ca571
James Wu <james_wu@appdet.com>
2021-04-05 16:30:12 +0800

    * custom carousel slide content in slot of Carousel

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template lang="pug">
-Carousel(:options="carouselOptions")/
+Carousel(:options="carouselOptions" class="carousel-container")
+  CarouselSlide(v-for="item in carouselData" :key="item" class="carousel-item") {{ item }}
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Carousel from './components/Carousel.vue'
+import CarouselSlide from './components/CarouselSlide.vue'
 
 export default defineComponent({
   name: 'App',
   setup() {
     const carouselOptions = {
-      gap: 10,
-      infinite: true,
-      // data: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
-      data: ['First']
+      infinite: true
     }
 
+    const carouselData = ['First-slide', 'Second-slide', 'Third-slide', 'Fourth-slide', 'Fifth-slide']
+
     return {
-      carouselOptions
+      carouselOptions,
+      carouselData
     }
   },
   components: {
-    Carousel
+    Carousel,
+    CarouselSlide
   }
 })
 </script>
@@ -31,5 +34,20 @@ export default defineComponent({
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+.carousel {
+  &-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80vw;
+    height: 80vh;
+  }
+
+  &-item {
+    background-color: #eee;
+  }
 }
 </style>
