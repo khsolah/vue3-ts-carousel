272ad7b7dad42de72d3c271a6505c8a0c25da2e5
James Wu <james_wu@appdet.com>
2021-04-05 16:27:27 +0800

    + define carousel-slide

---
+++ src/components/CarouselSlide.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.swiper-slide
+  slot(name="default")
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'CarouselSlide'
+})
+</script>
