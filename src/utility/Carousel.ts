926cc87c5d3170e4799e1248644e9b34c0843334
James Wu <james_wu@appdet.com>
2021-04-05 22:13:36 +0800

    + drag function

--- src/utility/Carousel.ts
+++ src/utility/Carousel.ts
@@ -6,6 +6,9 @@ export interface ICarousel {
   init(slideWidth: number, slideGap: number, slideQuantities: number): void
   slideTo(targetIndex: number): void
   transitionEnd(): void
+  startDragSlide(e: MouseEvent): void
+  draggingSlide(e: MouseEvent): void
+  endDragSlide(e: MouseEvent): void
 }
 
 export class Carousel implements ICarousel {
@@ -17,11 +20,17 @@ export class Carousel implements ICarousel {
   private _transitionActivated: boolean = false
   private _exceedLimit: boolean = false
 
-  /** constructor 
+  private _downPoint: number = 0
+  private _mousePoint: number = 0
+
+  /** constructor
    * @param {string} transitionDuration default: 300ms
    * @param {boolean} infinite infinite loop. default: false
-  */
-  constructor(public readonly infinite: boolean = false, public transitionDuration: string = '300ms') {
+   */
+  constructor(
+    public readonly infinite: boolean = false,
+    public transitionDuration: string = '300ms'
+  ) {
     if (infinite) this._index = 1
   }
 
@@ -32,14 +41,23 @@ export class Carousel implements ICarousel {
 
   // get 目前 wrapper 的 translate3d 的 x 值
   public get translate3dX(): number {
-    return -this._index * (this._slideWidth + this._slideGap)
+    return (
+      -this._index * (this._slideWidth + this._slideGap) -
+      this._mouseMoveDistance
+    )
   }
 
   public get transitionStyle() {
     return {
       transform: `translate3d(${this.translate3dX}px, 0, 0)`,
-      'transition-duration': this._transitionActivated ? this.transitionDuration : '0ms'
+      'transition-duration': this._transitionActivated
+        ? this.transitionDuration
+        : '0ms'
     }
+  }
+
+  private get _mouseMoveDistance() {
+    return this._downPoint - this._mousePoint
   }
 
   /** 設定初始值
@@ -47,7 +65,11 @@ export class Carousel implements ICarousel {
    * @param {number} slideGap slide 間距
    * @param {number} slideQuantities slide 數量
    */
-  public init(slideWidth: number, slideGap: number, slideQuantities: number): void {
+  public init(
+    slideWidth: number,
+    slideGap: number,
+    slideQuantities: number
+  ): void {
     this._slideWidth = slideWidth
     this._slideGap = slideGap
     this._slideQuantities = slideQuantities
@@ -60,11 +82,14 @@ export class Carousel implements ICarousel {
     if (this._transitionActivated) return
     this._transitionActivated = true
 
+    this._downPoint = this._mousePoint = 0
+
     if (!this.infinite) {
-      targetIndex = targetIndex < 0
-        ? -.1
-        : targetIndex >= this._slideQuantities
-          ? this._slideQuantities - .95
+      targetIndex =
+        targetIndex < 0
+          ? -0.1
+          : targetIndex >= this._slideQuantities
+          ? this._slideQuantities - 0.95
           : targetIndex
     }
 
@@ -76,14 +101,38 @@ export class Carousel implements ICarousel {
     this._transitionActivated = false
 
     if (this.infinite) {
-      this._index = this._index === 0
-        ? this._slideQuantities - 2
-        : this._index  === this._slideQuantities - 1
+      this._index =
+        this._index === 0
+          ? this._slideQuantities - 2
+          : this._index === this._slideQuantities - 1
           ? 1
           : this._index
     } else {
       if (this._index < 0) this.slideTo(0)
-      else if (this._index > this._slideQuantities - 1) this.slideTo(this._slideQuantities - 1)
+      else if (this._index > this._slideQuantities - 1) {
+        this.slideTo(this._slideQuantities - 1)
+      }
     }
+  }
+
+  public startDragSlide(e: MouseEvent) {
+    e.preventDefault()
+    this._downPoint = this._mousePoint = e.clientX
+
+    document.onmouseup = this.endDragSlide.bind(this)
+    document.onmousemove = this.draggingSlide.bind(this)
+  }
+  
+  public draggingSlide(e: MouseEvent) {
+    this._mousePoint = e.clientX
+    return
+  }
+
+  public endDragSlide (e: MouseEvent) {
+    e.preventDefault()
+    document.onmouseup = null
+    document.onmousemove = null
+    if (this._mouseMoveDistance < -99) this.slideTo(this._index - 1)
+    else if (this._mouseMoveDistance > 99) this.slideTo(this._index + 1)
   }
 }
